3c6e8bf4c8502fb410bd18352e6b4691e7a43301
Jiahe Wang <1995andywang@gmail.com>
2025-05-07 19:24:49 +0800

    feat: add [phone_number] backend validator in models.py

--- members/models.py
+++ members/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.core.validators import RegexValidator
 from django.db import models
 
 
@@ -11,7 +12,10 @@ class Member(models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(
+        max_length=20,
+        validators=[RegexValidator(r'^09\d{8}$', message='手機號碼格式錯誤')],
+    )
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     date_of_birth = models.DateField()
     line_id = models.CharField(max_length=64, null=True, blank=True)
