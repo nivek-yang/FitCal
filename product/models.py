b377ac7c0d16becfc2d1052bed33bd7b461ea6b8
sabrina <peiching913@gmail.com>
2025-05-08 20:17:22 +0800

    modify the field type

--- product/models.py
+++ product/models.py
@@ -7,8 +7,9 @@ from django.db import models
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
     name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='product_images', null=True, blank=True)
     description = models.TextField()
-    calories = models.IntegerField()
-    quantity = models.IntegerField()
+    calories = models.DecimalField(max_digits=5, decimal_places=1)
+    quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=0)
-    customize = models.TextField()
+    customize = models.TextField(null=True, blank=True)
