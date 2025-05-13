1d1d3206efcbeaf0149babb0a35715910222e0c7
Jiahe Wang <1995andywang@gmail.com>
2025-05-13 16:57:43 +0800

    refactor: change DatimeSelector from CDN to npm

--- src/scripts/flatpickr/main.js
+++ src/scripts/flatpickr/main.js
@@ -1,13 +1,18 @@
 import { StyledFlatpickr } from './init.js';
 
+export function initPickupTimePicker(
+  seletor,
+  style,
+  options = {
+    enableTime: true,
+    dateFormat: 'Y-m-d\\TH:i',
+    minuteIncrement: 10,
+    altInput: true,
+    altFormat: 'Y年m月d日 H:i',
+  },
+) {
+  new StyledFlatpickr(seletor, style, options);
+}
 // 範例: 套用 healthy_style 並覆寫選項
-new StyledFlatpickr('#id_pickup_time', 'healthy_style', {
-  enableTime: true,
-  dateFormat: 'Y-m-d\\TH:i',
-  minuteIncrement: 10,
-  altFormat: 'Y年m月d日',
-  altInput: true,
-  altFormat: 'Y年m月d日 H:i',
-});
 
 // 可在以下位置添加更多的 StyledFlatpickr 實例
--- src/scripts/input.js
+++ src/scripts/input.js
@@ -1,10 +1,15 @@
 import Alpine from 'alpinejs';
 import flatpickr from 'flatpickr';
 import 'htmx.org';
-import { StyledFlatpickr } from './components/StyledFlatpickr.js';
+import { initPickupTimePicker } from './flatpickr/main.js';
 
 window.Alpine = Alpine;
 window.flatpickr = flatpickr;
-window.StyledFlatpickr = StyledFlatpickr;
-
+initPickupTimePicker('#id_pickup_time', 'healthy_style');
+initPickupTimePicker('#id_date_of_birth', 'cool_style', {
+  enableTime: false,
+  dateFormat: 'Y-m-d',
+  altFormat: 'Y年m月d日',
+  altInput: true,
+});
 Alpine.start();
