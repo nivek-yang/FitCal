d98367861d0e8f2f55e3bae388d6af8305f800a9
hongshixian <a0984368815@gmail.com>
2025-05-15 14:58:12 +0800

    fix: remove app 'product' from settings.py

--- fitcal/settings.py
+++ fitcal/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 
 import environ
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,7 +41,6 @@ INSTALLED_APPS = [
     'users',
     'pages',
     'members',
-    'product',
     'orders',
     'products',
     'allauth',
